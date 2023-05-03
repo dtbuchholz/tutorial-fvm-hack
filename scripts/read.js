4342480ba1545283c0e9f79abc11938e44525e6b
Dan Buchholz <dbuchholz30@gmail.com>
2023-05-03 09:46:56 -0700

    fix: incomplete read script

--- scripts/read.js
+++ scripts/read.js
@@ -6,12 +6,20 @@ async function main() {
   const fvm = await ethers.getContractAt("FVM", contractAddr);
   const [signer] = await hre.ethers.getSigners();
   const chainId = await signer.getChainId();
+  const tableId = await fvm.tableId();
   const db = new Database({
-    baseUrl: helpers.getBaseUrl(1),
+    baseUrl: helpers.getBaseUrl(chainId),
   });
   const validator = new Validator(db.config);
-  const name = await validator.getTableById(21, 23);
-  console.log(name);
+  const { name } = await validator.getTableById({ chainId, tableId });
+  console.log(`Table '${name}' has the following data:`);
+  const query = `SELECT * FROM ${name}`;
+  const { results } = await db.prepare(query).all();
+  console.log(JSON.stringify(results, null, 2));
+  const encodeQuery = encodeURIComponent(query);
+  console.log(
+    `\nView this data here:\n${db.config.baseUrl}/query?statement=${encodeQuery}`
+  );
 }
 
 main().catch((error) => {
